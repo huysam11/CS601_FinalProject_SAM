fb1dfdf45168948ba319a836bb438635cd06d53f
Huy Sam <huyprolove114@gmail.com>
2023-12-11 17:41:09 -0500

    update script for index page

--- scripts/education.js
+++ scripts/education.js
@@ -1,24 +1,24 @@
-window.onload = function () {
+window.onload = () => {
     // Display an alert when the page has completely loaded
     alert('Please click anywhere to display the table');
   };
   
-  document.addEventListener('click', function () { //Click event
+  document.addEventListener('click', () => { //Click event
     const api = "./table.json";
     fetch(api)
-      .then(function (response) {
+      .then(response => {
         // Create a fetch request to return a promise
         if (!response.ok) {
           throw new Error(`Network response was not ok: ${response.status}`);
         }
         return response.json(); // Parse the JSON data from the response
       })
-      .then(function (data) {
+      .then(data => {
         // Log the status code and received data to the console 
         console.log(`Status Code: ${data.status}`);
         console.log('Received Data:', data);
         var col = "";
-        data.forEach(function (itemData) { // Create HTML string to display the data in a table
+        data.forEach(itemData => { // Create HTML string to display the data in a table
           col += "<tr>";
           col += "<td>" + itemData.coursename + "</td>";
           col += "<td>" + itemData.description + "</td>";
@@ -26,7 +26,7 @@ window.onload = function () {
         });
         document.getElementById('data').innerHTML = col; // Set inner HTML with id 'data'
       })
-      .catch(function (error) {
+      .catch(error => {
         console.error('Fetch Error:', error); // Handle errors
       });
   });
